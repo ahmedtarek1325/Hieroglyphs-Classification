6d248836163a760eb6fee3e455d54282b78b462b
ahmedtarek1325 <ahmedtarek1325>
2022-08-31 23:41:52 +0200

    adjust the rectangle printing..                                                                 > replaced the ys and xs on plotting the red rectangle

--- executer/visualizeImg.py
+++ executer/visualizeImg.py
@@ -3,8 +3,8 @@ import cv2
 def classes_on_img(img,predictions,regions):
     newimg= img.copy()
     for pred,reg in zip(predictions,regions):
-        cv2.rectangle(newimg, (reg[0], reg[2]), (reg[1], reg[3]), (0, 0, 255), 0)
-        cv2.putText(img=newimg, text=str(pred), org=(int(reg[0]) ,int(reg[2])),
+        cv2.rectangle(newimg, (reg[2], reg[0]), (reg[3], reg[1]), (0, 0, 255), 0)
+        cv2.putText(img=newimg, text=str(pred), org=(int(reg[2]) ,int(reg[0])),
                     fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=1, color=(0,255, 0),thickness=1)
 
     cv2.imwrite("results/out.png",newimg)
